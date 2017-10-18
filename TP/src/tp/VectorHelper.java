249ce000b5c5e6531825cfd949717d14dcbeb8c1
RandaB <fr_boukabene@esi.dz>
2017-10-18 04:47:33 +0100

    creation du constructeur de vector helper

---
+++ TP/src/tp/VectorHelper.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp;
+import java.util.*;
+/**
+ *
+ * @author block
+ */
+
+public class VectorHelper {
+    	private int taille;
+	private ArrayList<Integer> val  ;
+	
+	
+	
+	public VectorHelper(int a) {
+		// TODO Auto-generated constructor stub
+		taille=a;
+		this.val=new ArrayList<Integer>();
+		
+	}
+    
+}
