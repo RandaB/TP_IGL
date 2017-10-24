7e879ea871ca0cc3ba3e0ef495f008093ee1cd55
terro07 <fa_ouali@esi.dz>
2017-10-24 06:09:39 +0100

    l'ajout des test unitaire
    ajouter des test unitaire

---
+++ TP/test/tp_01/Tp_01Suite.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_01;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author pc
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({tp_01.TP_01Test.class, tp_01.EqualExceptionTest.class, tp_01.VectorHelperTest.class})
+public class Tp_01Suite {
+
+    /**
+     *
+     * @throws Exception
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
